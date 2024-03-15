176662d926b72625fb8ab7aad97a4dbcec97fc8b
vsaikanakadandi <162981346+vsaikanakadandi@users.noreply.github.com>
2024-03-15 16:37:32 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,19 @@
+#s1="HeY TherE you are"
+#count=0
+#for i in s1:
+ #   if i.islower():
+  #      count+=1
+#print(count)
+
+
+s1= "IMAGIN"
+fst= s1[0]
+bal= s1[1:]
+txt=bal.replace(fst, "$")
+print(fst+txt)
+
+s1 ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+char=len(s1)
+print(char)
+print(char.format)
+
